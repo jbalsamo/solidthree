e8824d05b8a13be0ad00ff3796b58b07508f3adf
Joseph Balsamo <joseph.balsamo@stonybrook.edu>
2023-07-06 14:26:38 -0400

    initall commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import type { Component } from "solid-js";
+
+import styles from "./css/App.module.css";
+import logo from "./assets/three.png";
+
+const App: Component = () => {
+  return <div class={styles.App}></div>;
+};
+
+export default App;
